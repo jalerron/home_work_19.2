2ce36d05dc70639045907d69dbf5277a8438fcef
Ivan Nikitin <ivannikitin9853@gmail.com>
2023-12-16 15:46:07 +0700

    made changes

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+# admin.site.register(Product)
+# admin.site.register(Category)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price_for_one', 'description',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
