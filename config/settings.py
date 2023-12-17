89ba73d485884159629b8782b1cc350fd9ea46ee
Ivan Nikitin <ivannikitin9853@gmail.com>
2023-12-17 10:14:17 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'skystore',
         'USER': 'postgres',
-        'PASSWORD': 'Swtbme666^^^'
+        'PASSWORD': 'Введите свой пароль'
     }
 }
 
