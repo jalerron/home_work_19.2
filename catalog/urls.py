42e7edb781eb5ea63be505e32fe5202187bd7ebb
Ivan Nikitin <ivannikitin9853@gmail.com>
2023-12-30 11:19:49 +0700

    отзывы отображаются для отдельного продукта

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from catalog.views import index, contacts, ProductListView, ProductDetailView
 from catalog.apps import CatalogConfig
+from reviews.views import ReviewsListView
 
 app_name = CatalogConfig.name
 
@@ -9,6 +10,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('catalog/', ProductListView.as_view(), name='product_list'),
     path('contacts/', contacts, name='contacts'),
-    # path('catalog//', product_detail, name='product_detail')
-    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail')
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('reviews/<int:product_id>/', ReviewsListView.as_view(), name='list_reviews')
 ]
