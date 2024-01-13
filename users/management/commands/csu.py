cb92ca0c7959714be78722628e828cc3cd723071
jalerron <ivannikitin9853@gmail.com>
2024-01-13 10:40:39 +0700

    добавил команду создания суперпользователя по email

---
+++ users/management/commands/csu.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            username='admin',
+            email='admin@gmail.com',
+            first_name='Admin',
+            last_name='Admin',
+            is_staff=True,
+            is_superuser=True,
+        )
+
+        user.set_password('123qweASD')
+        user.save()
--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from catalog.models import NULLABLE
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='почта')
-
+    
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     country = models.CharField(max_length=15, verbose_name='страна', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
