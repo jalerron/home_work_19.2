29d1d090470059d36e0709dd8d736c1199274c1b
jalerron <ivannikitin9853@gmail.com>
2024-01-15 15:32:50 +0700

    add files

--- users/models.py
+++ users/models.py
@@ -7,7 +7,10 @@ from catalog.models import NULLABLE
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='почта')
-    
+
+    is_active = models.BooleanField(default=False)
+    email_verify = models.BooleanField(default=False, verbose_name='верификация почты')
+
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     country = models.CharField(max_length=15, verbose_name='страна', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
